17d1ebb3b9b035023c36d8abbeec7d441b3dedc0
kimjayluta <kimjayluta0721@gmail.com>
2020-07-08 20:43:57 +0800

    chore: Added a function that save the captured photo

--- src/hooks/usePhotoGallery.tsx
+++ src/hooks/usePhotoGallery.tsx
@@ -5,8 +5,10 @@ import { useStorage } from '@ionic/react-hooks/storage';
 import { isPlatform } from '@ionic/react';
 import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
 
+
 export function usePhotoGallery() {
 	const { getPhoto } = useCamera();
+	const [photos, setPhotos] = useState<Photo[]>([]);
 	
 	const takePhoto = async () => {
 		const cameraPhoto = await getPhoto({
@@ -14,9 +16,26 @@ export function usePhotoGallery() {
 			source: CameraSource.Camera,
 			quality: 100
 		});
+
+		const fileName = new Date().getTime() + '.jpeg';
+		
+		const newPhotos = [{
+			filepath: fileName,
+			webviewPath: cameraPhoto.webPath
+		}, ...photos];
+		
+		setPhotos(newPhotos)
+
 	};
 
 	return {
+		photos,
 		takePhoto
 	};
+}
+
+export interface Photo {
+	filepath: string;
+	webviewPath?: string;
+	base64?: string;
 }
